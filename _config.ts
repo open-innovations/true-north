5089c4518167972af4c192728e9b47c3c484520a
slowe <299787+slowe@users.noreply.github.com>
2024-05-09 16:29:51 +0100

    Update to Lume 2 and add OI Lume Viz

--- _config.ts
+++ _config.ts
@@ -1,14 +1,35 @@
 import lume from "lume/mod.ts";
+import nunjucks from "lume/plugins/nunjucks.ts";	// Lume 2.0 requires us to add Nunjucks
 import base_path from "lume/plugins/base_path.ts";
 import date from "lume/plugins/date.ts";
 import metas from "lume/plugins/metas.ts";
 import postcss from "lume/plugins/postcss.ts";
 
+// Importing the OI Lume charts and utilities
+import oiViz from "https://deno.land/x/oi_lume_viz@v0.15.1/mod.ts";
+import autoDependency from "https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts";
+import csvLoader from "https://deno.land/x/oi_lume_utils@v0.4.0/loaders/csv-loader.ts";
+import jsonLoader from "lume/core/loaders/json.ts";
+
+
 const site = lume({
   src: './src',
   // TODO Update this with the proper URL
-  location: new URL("https://open-innovations.github.io/oi-lume-template/"),
+  location: new URL("https://open-innovations.github.io/true-north/"),
 });
+
+// Need to explicitly include it for Lume 2
+site.use(nunjucks());
+
+site.loadData([".csv", ".tsv", ".dat"], csvLoader({ basic: true }));
+site.loadData([".geojson"], jsonLoader);
+site.loadData([".hexjson"], jsonLoader);
+
+
+// Import lume viz
+import oiVizConfig from "./oi-viz-config.ts";
+site.use(oiViz(oiVizConfig));
+
 
 site.use(base_path());
 site.use(metas({
