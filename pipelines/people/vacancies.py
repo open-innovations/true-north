a175cd43400139f2472385088908617dfcb653dd
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-05-16 16:08:16 +0100

    Add growth by sector bar chart

--- pipelines/people/vacancies.py
+++ pipelines/people/vacancies.py
@@ -2,16 +2,24 @@ from pipelines.util import *
 
 if __name__ == "__main__":
 
-    # # Load data
-    # data = etl_load(WDIR, 'vacancies/vacancies-growth-by-sector.csv')
-    # # Write to csv
-    # etl_write(data, os.path.join(DATA_DIR, 'vacancies/growth_by_sector'))
     data = etl_load(WDIR, 'cs/vacancies_by_sector_percentage_change_on_previous.csv')
+
     data = etl.cut(data, 'dates.date', 'value', 'variable.name')
-    most_recent_date = max(etl.values(data, 'dates.date'))
-    data = etl.selecteq(data, 'dates.date', most_recent_date)
+
+    #most_recent_date = max(etl.values(data, 'dates.date'))
+    # last_three_months = list(item for item in dates)[-3:]
+    # print(last_three_months)
+    #data = etl.selecteq(data, 'dates.date', most_recent_date)
+
     data = etl.recast(data, key='dates.date', variablefield='variable.name', valuefield='value')
+
     data = etl.setheader(data, split_text(on="-", headers=etl.header(data)))
+
+    data = etl.setheader(data, slugify_column_names(etl.header(data)))
+
+    # transpose the table for the bar chart visualisation
+    data = etl.transpose(data).rename({"dates_date": "sector"})
+
     etl_write(data, os.path.join(SRC_DIR, 'themes/people-skills-future/_data/growth_by_sector.csv'))
     
     print('Finished vacancies')
