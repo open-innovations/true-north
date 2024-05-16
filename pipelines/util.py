c9c42502d553e790cbfbe78f87773eaa2b0a7236
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-05-16 15:16:15 +0100

    Update util.py

--- pipelines/util.py
+++ pipelines/util.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 import petl as etl
+from slugify import slugify
 
 #set some directory paths
 THIS_DIR = os.path.dirname(__file__)
@@ -8,6 +9,7 @@ TOP = os.path.abspath(os.path.join(THIS_DIR, os.pardir))
 
 WDIR = os.path.join(TOP, 'working')
 DATA_DIR = os.path.join(TOP, 'src/_data')
+SRC_DIR = os.path.join(TOP, 'src')
 METADATA_DIR = os.path.join(TOP, 'metadata')
 
 def etl_load(working, fname):
@@ -24,3 +26,16 @@ def iso_to_unix(row):
     iso_date = row['date']
     dt = datetime.fromisoformat(iso_date)
     return int(dt.timestamp())
+
+def slugify_column_names(headers):
+    return [slugify(header, separator='_') for header in headers]
+
+def split_text(on, headers):
+    l = []
+    for header in headers:
+        try:
+            txt = str(header).split(on)[1]
+            l.append(txt)
+        except:
+            l.append(header) 
+    return l
