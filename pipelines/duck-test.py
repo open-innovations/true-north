7119832c3489163a619b4753c6ae3e64224e534c
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-05-20 17:40:01 +0100

    Remove deprecated files

--- pipelines/duck-test.py
+++
@@ -1,9 +0,0 @@
-import duckdb
-
-con = duckdb.connect()
-
-url = 'https://github.com/economic-analytics/edd/raw/main/data/parquet/UCST.parquet'
-
-data = con.execute(f"SELECT * FROM read_parquet('{url}')").fetchdf()
-
-print(data)
