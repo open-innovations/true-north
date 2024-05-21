f592293135183bee6d326bad67060a9de53038d9
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-05-21 10:23:39 +0100

    Tweaks to gva pipeline

--- pipelines/sustainable/gva.py
+++ pipelines/sustainable/gva.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
 
     # @TODO write a generalised function of below
     # @TODO figure out why MAX('date') won't work
-    data = con.execute(f"SELECT \"dates.date\" AS date, \"variable.name\", \"geography.code\", \"industry.name\", value FROM '{URL}' WHERE \"industry.name\"=='All industries' and \"variable.name\"=='GVA Current Prices £m' and date='2022-01-01';").fetchdf()
+    data = con.execute(f"SELECT \"dates.date\" AS date, \"variable.name\", \"geography.code\", \"industry.name\", value FROM '{URL}' WHERE \"industry.name\"=='All industries' AND date='2022-01-01';").fetchdf()
 
     # possible to rewrite below in the query, 
     # which may be quicker, but not sure if it's
     # worth it given its easier for me to do in pandas.
     data = data.pivot(columns='variable.name', values='value', index='geography.code')
-    data.rename(columns={'GVA Current Prices £m': 'gva_current_prices'}, inplace=True)
+    #data.rename(columns={'GVA Current Prices £m': 'gva_current_prices', 'GVA Constant Prices £m': 'gva_constant_prices'}, inplace=True)
     data.to_csv(os.path.join(SRC_DIR, 'themes/sustainable-growth/_data/gva_lad.csv'))
