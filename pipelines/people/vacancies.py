892b6e8c1b2fd4ab7605e8b8ae6a7b8a7fc29bf5
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-05-24 17:47:31 +0100

    Vacancy (thousands) by sector. #2

--- pipelines/people/vacancies.py
+++ pipelines/people/vacancies.py
@@ -1,6 +1,6 @@
 from pipelines.util import *
-
-if __name__ == "__main__":
+import duckdb
+def vacancies_growth():
 
     data = etl_load(WDIR, 'cs/vacancies_by_sector_percentage_change_on_previous.csv')
 
@@ -16,5 +16,20 @@ if __name__ == "__main__":
     data = etl.transpose(data).rename({"dates_date": "sector"})
 
     etl_write(data, os.path.join(SRC_DIR, 'themes/people-skills-future/_data/growth_by_sector.csv'))
-    
+
+    return
+
+def vacancies_by_sector():
+    URL = "https://github.com/economic-analytics/edd/raw/main/data/parquet/LMS.parquet"
+    con = duckdb.connect()
+    start_code = 'JP9H'
+    end_code = 'JP9Y'
+    codes = [start_code[:-1] + chr(char) for char in range(ord(start_code[-1]), ord(end_code[-1]) + 1 )]
+    data = con.execute(f"SELECT \"dates.date\" AS date, \"variable.name\", \"variable.unit\", \"variable.code\", value  FROM '{URL}' WHERE \"dates.freq\"=='m'").fetchdf()
+    data = data[data['variable.code'].isin(codes)]
+    data.to_csv(os.path.join(SRC_DIR,'themes/people-skills-future/_data/vacancies_by_sector.csv'))
+
+if __name__ == "__main__":
+    vacancies_growth()
+    vacancies_by_sector()
     print('Finished vacancies')
