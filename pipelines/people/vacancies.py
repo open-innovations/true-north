aebf6d122d3f4beafc6c61ea6d01105226e4a1e9
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-05-28 12:17:33 +0100

    Add total vacancies time series and yearly % change. Closes #2 Closes #4

--- pipelines/people/vacancies.py
+++ pipelines/people/vacancies.py
@@ -1,5 +1,6 @@
 from pipelines.util import *
 import duckdb
+
 def vacancies_growth():
 
     data = etl_load(WDIR, 'cs/vacancies_by_sector_percentage_change_on_previous.csv')
@@ -22,14 +23,43 @@ def vacancies_growth():
 def vacancies_by_sector():
     URL = "https://github.com/economic-analytics/edd/raw/main/data/parquet/LMS.parquet"
     con = duckdb.connect()
+
+    # make a list of codes from JP9H to JP9Y
     start_code = 'JP9H'
     end_code = 'JP9Y'
     codes = [start_code[:-1] + chr(char) for char in range(ord(start_code[-1]), ord(end_code[-1]) + 1 )]
+
+    # get the required columns and select those with "variable.name" in "codes".
     data = con.execute(f"SELECT \"dates.date\" AS date, \"variable.name\", \"variable.unit\", \"variable.code\", value  FROM '{URL}' WHERE \"dates.freq\"=='m'").fetchdf()
     data = data[data['variable.code'].isin(codes)]
+
+    # Remove recurring string from "variable.name" column
+    data['variable.name'] = data['variable.name'].str.replace(pat="UK Job Vacancies (thousands) - ", repl="")
+
+    # pivot data to wide format for visualisation
+    data = data.pivot(index='date', columns='variable.name', values='value')
+    
+    # limit the time series to last 10 years -> 12months x 10 years = 120 values.
+    data = data.tail(120)
+
     data.to_csv(os.path.join(SRC_DIR,'themes/people-skills-future/_data/vacancies_by_sector.csv'))
+    return data
+
+def yearly_change_by_sector(data):
+    # Get yearly data going back from most recent data, then flip to put in correct order.
+    data = data.iloc[-1::-12, :].iloc[::-1].copy()
+
+    # iterate through each column to work out pct change.
+    for col in data.columns.to_list():
+        data[f'{col}'] = data[f'{col}'].pct_change().mul(100).round(1)
+    # transpose data to make a bar chart
+    data = data.T
+    data.index.rename('sector', inplace=True)
+    data.to_csv(os.path.join(SRC_DIR, 'themes/people-skills-future/_data/vacancies_yearly_change_by_sector.csv'))
+    return
 
 if __name__ == "__main__":
     vacancies_growth()
-    vacancies_by_sector()
+    data = vacancies_by_sector()
+    yearly_change_by_sector(data)
     print('Finished vacancies')
