d943fa62378c5cf1c8d21d64ca623db19933e3df
Christian Spence <mail@christianspence.co.uk>
2024-06-05 13:22:52 +0100

    Add fasting growing sector by ITL3 geog

--- working/cs/gva-growth.R
+++ working/cs/gva-growth.R
@@ -29,3 +29,16 @@ rgva |>
   dplyr::group_by(geography.code, geography.name) |>
   dplyr::filter(growth_2019_2022 == max(growth_2019_2022)) |>
   readr::write_csv("working/cs/fasting-growing-sector-by-itl2.csv")
+
+rgva |>
+  dplyr::filter(geography.type == "ITL3") |>
+  dplyr::filter(grepl("^TL[CDE]", geography.code)) |>
+  dplyr::filter(variable.name == "Constant prices") |>
+  dplyr::filter(grepl("^[A-Z]{1} ", industry.code)) |>
+  dplyr::filter(dates.date == "2019-01-01" | dates.date == "2022-01-01") |>
+  dplyr::collect() |>
+  tidyr::pivot_wider(names_from = dates.date, values_from = value) |>
+  dplyr::mutate(growth_2019_2022 = (`2022-01-01` - `2019-01-01`) / `2019-01-01` * 100) |>
+  dplyr::group_by(geography.code, geography.name) |>
+  dplyr::filter(growth_2019_2022 == max(growth_2019_2022)) |>
+  readr::write_csv("working/cs/fasting-growing-sector-by-itl3.csv")
