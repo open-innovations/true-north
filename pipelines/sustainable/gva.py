e6be03efd9f64e1cfed7720dc678ce446fb90280
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-06-05 15:08:27 +0100

    Add fastest growing sectors by LAD

--- pipelines/sustainable/gva.py
+++ pipelines/sustainable/gva.py
@@ -1,7 +1,13 @@
 import duckdb
 from pipelines.util import *
+import pandas as pd
 
 URL = 'https://github.com/economic-analytics/edd/raw/main/data/parquet/RGVA_LAD.parquet'
+
+def fastest_growing_sector():
+    data = pd.read_csv(os.path.join(WDIR, 'cs/fasting-growing-sector-by-lad23.csv'), usecols=['geography.code', 'industry.name', '2019-01-01', '2022-01-01', 'growth_2019_2022'])
+    data.to_csv(os.path.join(SRC_DIR, 'themes/sustainable-growth/gva/_data/fastest_growing_sector_by_LAD23.csv'))
+    return
 
 if __name__ == "__main__":
     con = duckdb.connect()
@@ -15,4 +21,6 @@ if __name__ == "__main__":
     # worth it given its easier for me to do in pandas.
     data = data.pivot(columns='variable.name', values='value', index='geography.code')
     #data.rename(columns={'GVA Current Prices £m': 'gva_current_prices', 'GVA Constant Prices £m': 'gva_constant_prices'}, inplace=True)
-    data.to_csv(os.path.join(SRC_DIR, 'themes/sustainable-growth/_data/gva_lad.csv'))
+    data.to_csv(os.path.join(SRC_DIR, 'themes/sustainable-growth/gva/_data/gva_lad.csv'))
+
+    fastest_growing_sector()
