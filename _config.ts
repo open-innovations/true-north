33bc24e756d431ac935d8c314b2db218e937414e
slowe <299787+slowe@users.noreply.github.com>
2024-06-11 11:04:55 +0100

    Fix #46

--- _config.ts
+++ _config.ts
@@ -7,7 +7,7 @@ import postcss from "lume/plugins/postcss.ts";
 
 
 // Importing the OI Lume charts and utilities
-import oiViz from "https://deno.land/x/oi_lume_viz@v0.15.3/mod.ts";
+import oiViz from "https://deno.land/x/oi_lume_viz@v0.15.5/mod.ts";
 import autoDependency from "https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts";
 import csvLoader from "https://deno.land/x/oi_lume_utils@v0.4.0/loaders/csv-loader.ts";
 import jsonLoader from "lume/core/loaders/json.ts";
