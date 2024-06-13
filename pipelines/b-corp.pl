8578ccc5c76a8e6f028330594a756c8931dc6f8f
slowe <299787+slowe@users.noreply.github.com>
2024-06-13 11:08:31 +0100

    Update B Corp data

--- pipelines/b-corp.pl
+++ pipelines/b-corp.pl
@@ -341,6 +341,7 @@ sub fixPostcode {
 	if($pcd eq "UK" && $name eq "By Sarah"){ $pcd = "B798RH"; }
 	if($pcd eq "WC2A0HF"){ $pcd = "WC2H0HF"; }
 	if($pcd eq "TA11FH"){ $pcd = "TA11QN"; }
+	if($pcd eq "SE28EA" && $name eq "ZeroBees"){ $pcd = "SE228EA"; }
 	return $pcd;
 }
 
