92052f101a3612d391ae7389ee27157b1525f768
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-06-18 13:32:26 +0100

    Update northern stars

--- pipelines/truenorth/northern_stars.py
+++
@@ -1,8 +0,0 @@
-from pipelines.util import *
-import pandas as pd
-
-if __name__ == "__main__":
-    data = pd.read_excel(os.path.join(WDIR, 'true-north/northern_stars.xlsx'))
-    # sort values by date, descending
-    data.sort_values('date_published', inplace=True, ascending=False)
-    data.to_csv(os.path.join(SRC_DIR, '_data/dashboard/northern_stars.csv'))
