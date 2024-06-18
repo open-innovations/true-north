90510da2fcdab26cea0ec9ff4ce6409a703929a0
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-06-18 14:03:35 +0100

    change nvq level 3 to level 4

--- pipelines/people/qualifications.py
+++ pipelines/people/qualifications.py
@@ -4,7 +4,7 @@ def nvq():
     # Load data
     data = etl_load(WDIR, 'cs/cs-true-north.csv')
     # Filter data
-    data = etl.select(data, lambda rec: rec.Theme == 'qualifications' and rec.measures_name == 'Variable' and rec.variable_name == '% with NVQ3+ - aged 16-64')
+    data = etl.select(data, lambda rec: rec.Theme == 'qualifications' and rec.measures_name == 'Variable' and rec.variable_name == '% with NVQ4+ - aged 16-64')
     # Drop "NA" values.
     data = etl.select(data, lambda row: all(v != 'NA' for v in row))
     # Keep only necessary columns
@@ -14,7 +14,7 @@ def nvq():
     # Convert the iso dates to unix
     data = etl.addfield(data, 'unix_timestamp', iso_to_unix)
     # Write to file
-    etl_write(data, os.path.join(SRC_DIR, 'themes/people-skills-future/_data/nvq_3plus.csv'))
+    etl_write(data, os.path.join(SRC_DIR, 'themes/people-skills-future/_data/nvq_4plus.csv'))
     return 
 
 if __name__ == "__main__":
