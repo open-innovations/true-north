ebcc021dcec43d17a8de44963733bdb87d5b8ba0
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-06-24 14:20:52 +0100

    Update vacancies

--- pipelines/people/vacancies.py
+++ pipelines/people/vacancies.py
@@ -1,25 +1,4 @@
 from pipelines.util import *
-import duckdb
-import pandas as pd
-
-def vacancies_growth():
-
-    data = etl_load(WDIR, 'cs/vacancies_by_sector_percentage_change_on_previous.csv')
-
-    data = etl.cut(data, 'dates.date', 'value', 'variable.name')
-
-    data = etl.recast(data, key='dates.date', variablefield='variable.name', valuefield='value')
-
-    data = etl.setheader(data, split_text(on="-", headers=etl.header(data)))
-
-    data = etl.setheader(data, slugify_column_names(etl.header(data)))
-
-    # transpose the table for the bar chart visualisation
-    data = etl.transpose(data).rename({"dates_date": "sector"})
-
-    etl_write(data, os.path.join(SRC_DIR, 'themes/people-skills-future/_data/growth_by_sector.csv'))
-
-    return
 
 def vacancies_by_sector():
     URL = "https://github.com/economic-analytics/edd/raw/main/data/parquet/LMS.parquet"
@@ -67,7 +46,6 @@ def yearly_change_by_sector(data):
     return
 
 if __name__ == "__main__":
-    vacancies_growth()
     data = vacancies_by_sector()
     yearly_change_by_sector(data)
     print('Finished vacancies')
