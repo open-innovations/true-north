abae37489b140bfc79f8f3016e2f27410389c06e
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-06-26 16:18:45 +0100

    Update GVA

--- pipelines/sustainable/gva.py
+++ pipelines/sustainable/gva.py
@@ -2,22 +2,25 @@ import duckdb
 from pipelines.util import *
 
 URL = 'https://github.com/economic-analytics/edd/raw/main/data/parquet/RGVA_LAD.parquet'
+query = f"SELECT \"dates.date\" AS date, \"variable.name\", \"geography.code\", \"industry.name\", value FROM '{URL}' WHERE \"industry.name\"=='All industries';"
 
 def gva_by_local_authority():
-    con = duckdb.connect()
+    data = remote_parquet_as_dataframe(query)
 
-    # @TODO write a generalised function of below
-    # @TODO figure out why MAX('date') won't work
-    data = con.execute(f"SELECT \"dates.date\" AS date, \"variable.name\", \"geography.code\", \"industry.name\", value FROM '{URL}' WHERE \"industry.name\"=='All industries';").fetchdf()
+    # filter the frame
+    data = data[(data['variable.name'] == 'GVA Current Prices £m')]
 
-    # possible to rewrite below in the query, 
-    # which may be quicker, but not sure if it's
-    # worth it given its easier for me to do in pandas.
-    data = data[(data['variable.name'] == 'GVA Current Prices £m') & (data['date'] == max(data['date']))]
+    # get the most recent date
+    data = most_recent_date(data, 'date')
+    
+    # pivot the frame
     data = data.pivot(columns='variable.name', values='value', index='geography.code')
-    #data.rename(columns={'GVA Current Prices £m': 'gva_current_prices', 'GVA Constant Prices £m': 'gva_constant_prices'}, inplace=True)
+    
+    # write to csv
     data.to_csv(os.path.join(SRC_DIR, 'themes/sustainable-growth/gva/_data/gva_lad.csv'))
+    return 
 
 if __name__ == "__main__":
     gva_by_local_authority()
     time_updated(os.path.join(SRC_DIR, 'themes/sustainable-growth/gva/index.vto'), 'nicetheme:')
+    edd_last_updated_next_updated(id='RGVA_LAD')
